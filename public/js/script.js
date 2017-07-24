062a3349a18f58f46cce300de355ae4551541585
Khang Van <vanthaikhang@gmail.com>
2017-07-24 10:45:46 +0200

    updated public/js/script.js

--- public/js/script.js
+++ public/js/script.js
@@ -1,1 +1,30 @@
 var data = {"name":"here show mean js working"};
+
+var srvrunon = 'localhost';
+var srvpointo = 'localhostz';
+var url = `http://${srvrunon}/sql/${srvpointo}/`;
+
+
+function logSerial()
+{
+    // Thuộc tính
+    this.serial = '';
+    this.po = '';
+this.line = '';
+this.opcode = '';
+     
+    // Phương thức
+    this.setInfo = function(serial, po, line, opcode){
+ // Thuộc tính
+     this.serial = serial;
+     this.po = po;
+ this.line = line;
+ this.opcode = opcode;
+    };
+     
+    this.log = function(){
+       // do something here
+
+        return "ok";
+    }
+}
