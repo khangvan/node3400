7c10caf17859d9884a6b3575ef33df67bc4b66fc
Van <khang.van@datalogic.com>
2018-01-25 09:28:39 +0700

    config server for OpenShift

--- server.js
+++ server.js
@@ -902,3 +902,12 @@ app.listen(app.get('port'), function() {
 //     key: fs.readFileSync('key.pem'),
 //     cert: fs.readFileSync('cert.pem')
 //   }, app).listen(55555);
+
+var server = express();
+
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+ 
+server.listen(server_port, server_ip_address, function () {
+  console.log( "Listening on " + server_ip_address + ", port " + server_port )
+});
